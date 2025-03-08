b75bd9d76b3ab0a6aaf4958a7295404c26dbaf16
James Leo <ocampojamesleo04@gmail.com>
2025-03-08 10:08:53 +0800

    font tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import ClientStory from "@/components/server/ClientStory";
+import ClientSection from "@/components/server/ClientSection";
 import GuestPage from "@/components/server/GuestPage";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-items-center gap-16">
       <GuestPage />
-      <ClientStory />
+      <ClientSection />
     </div>
   );
 }
