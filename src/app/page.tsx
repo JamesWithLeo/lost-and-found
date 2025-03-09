dccda9e20c75c9ab4efce496a3e1af5bb244f223
James Leo <ocampojamesleo04@gmail.com>
2025-03-09 11:28:25 +0800

    page error tweaks v1.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import GuestPage from "@/components/server/GuestPage";
 interface PageProps {
   searchParams?: Record<string, string | string[]>;
 }
-export default async function Home({ searchParams }: PageProps) {
+export default function Home({ searchParams }: PageProps) {
   const openSearch = searchParams?.new === "true";
   return (
     <div className="flex min-h-screen flex-col items-center justify-items-center">
