ec59ec94c4bcddf1659d3380aa4ce6dfd8caa874
James Leo <ocampojamesleo04@gmail.com>
2025-03-09 12:01:07 +0800

    page tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import GuestPage from "@/components/server/GuestPage";
 interface PageProps {
-  searchParams?: Record<string, string | string[]>;
+  searchParams?: Promise<Record<string, string | string[]>>;
 }
 
-export default function Page({ searchParams }: PageProps) {
-  const openSearch = searchParams?.new === "true";
+export default async function Page({ searchParams }: PageProps) {
+  const params = await searchParams;
+  const openSearch = params?.new === "true";
   return (
     <div className="flex min-h-screen flex-col items-center justify-items-center">
       <GuestPage isModalOpen={openSearch} />
