7a030b14d5d12cae52e42cb336528179eb54bc5c
James Leo <ocampojamesleo04@gmail.com>
2025-03-19 16:25:43 +0800

    exit modal fixed

--- src/ui/server/SearchModal.tsx
+++ src/ui/server/SearchModal.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ExitModal from "../exitModal";
 
 export default function SearchModal() {
   return (
@@ -6,17 +7,7 @@ export default function SearchModal() {
       <div className="fixed inset-0 z-40 h-full w-full bg-black opacity-45"></div>
       <div className="fixed left-1/2 top-1/2 z-50 flex h-96 w-96 -translate-x-1/2 -translate-y-1/2 transform flex-col gap-10 rounded-xl bg-white p-4">
         <div className="flex flex-col items-end">
-          <Link href={"/"}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="22"
-              fill="#000000"
-              viewBox="0 0 256 256"
-            >
-              <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
-            </svg>
-          </Link>
+          <ExitModal />
         </div>
         <h1 className="text-center">Report Item</h1>
         <div className="flex w-full justify-evenly">
