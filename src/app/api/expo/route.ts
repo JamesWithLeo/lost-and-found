71b3bb5d4b4fbaee2a63afc980ca82ebad7297dd
James Leo <ocampojamesleo04@gmail.com>
2025-04-08 11:46:32 +0800

    fix GET v1.2

--- src/app/api/expo/route.ts
+++ src/app/api/expo/route.ts
@@ -1,29 +1,8 @@
 import { getUsersByProvider } from "@/db/drizzle";
 import { NextResponse } from "next/server";
 
-const allowedOrigins = [
-  "http://localhost:8081", // Expo local dev
-  "https://lost-and-found-chi.vercel.app", // Deployed frontend
-];
-
-function getCorsHeaders(origin: string | null): HeadersInit {
-  const headers: HeadersInit = {
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    "Content-Type": "application/json",
-  };
-
-  if (origin && allowedOrigins.includes(origin)) {
-    headers["Access-Control-Allow-Origin"] = origin;
-  }
-
-  return headers;
-}
-
 export async function GET(req: Request) {
   const userEmail = req.headers.get("x-user-email");
-  const orign = req.headers.get("origin");
-  const headers = getCorsHeaders(orign);
   // Check if the header is present
 
   if (!userEmail) {
@@ -31,7 +10,10 @@ export async function GET(req: Request) {
       { error: "Missing user email header" },
       {
         status: 400,
-        headers,
+        headers: {
+          "Content-type": "application/json",
+          "Access-Control-Allow-Headers": "*",
+        },
       },
     );
   }
@@ -42,7 +24,10 @@ export async function GET(req: Request) {
     { email: userEmail, user: dbUser },
     {
       status: 200,
-      headers,
+      headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Headers": "*",
+      },
     },
   );
 }
